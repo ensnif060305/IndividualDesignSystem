cce3fe45f8ac45c9b0a027dbcab0023145ed271c
ensnif060305 <jmsun0305@gmail.com>
2023-12-07 21:41:33 +0900

    fix: version 수정

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 /**
- * @ensnif/ui v0.0.46
+ * @ensnif/ui v0.0.67
  *
  * @license MIT
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
  */
 "use client";
 export * from "./colors";
